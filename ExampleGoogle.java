d015290fccfebc51a0324d8946916150bb720ccb
andishpan <73879298+andishpan@users.noreply.github.com>
2021-07-17 19:57:03 +0200

    Update ExampleGoogle.java

--- ExampleGoogle.java
+++ ExampleGoogle.java
@@ -1,6 +1,9 @@
 
 
 
+package bigigoogle;
+
+
 import com.opencsv.CSVReader;
 import com.opencsv.exceptions.CsvValidationException;
 
@@ -8,6 +11,8 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -15,9 +20,14 @@ import java.util.List;
 
 
 import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 import static spark.Spark.*;
 import spark.template.jade.JadeTemplateEngine;
 import spark.ModelAndView;
+
+import javax.servlet.MultipartConfigElement;
 
 public class ExampleCSV {
 
@@ -110,6 +120,8 @@ public class ExampleCSV {
                 comics.add(comic);
 
 
+
+
             }
 
         } catch (IOException ioe) {
@@ -118,6 +130,56 @@ public class ExampleCSV {
 
         return comics;
     }
+
+
+  /*  private static Map<String, Object> checkCSV() throws FileNotFoundException {
+
+       // List<ExampleCSV> comics = new ArrayList<>();
+        Scanner input = new Scanner(System.in);
+        String strFile = "C:\\Files\\XKCD.csv";
+        String line = "";
+        List<String> result = new ArrayList<String>();
+        Map<String, Object> map = new HashMap<String, Object>();
+
+        BufferedReader br = new BufferedReader(new FileReader(strFile));
+
+
+        try {
+
+            while ((line = br.readLine()) != null) {
+
+
+                String[] attributes = line.split(",(?=(?:[^\"]*\"[^\"]*\")*(?![^\"]*\"))");
+
+
+
+                for(int i = 0; i < attributes.length; i++) {
+                    if (!map.containsKey(attributes[i])) {
+
+
+                        result.add(line);
+                        map.put(attributes[i], result);//assigns a key to an ArrayList that stores the value
+                    } else {
+
+                        result = (ArrayList<String>) map.get(attributes[i]);
+                        result.add(line);
+                    }
+
+
+
+                }
+
+
+            }
+
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+        map.get(input.nextLine());
+
+        return map;
+
+    }    */
 
 
     private static ExampleCSV createComic(String[] metadata) {
@@ -135,13 +197,22 @@ public class ExampleCSV {
         List<ExampleCSV> comics = readComicsFromCSV();
 
 
+
+
+
+
+            // output.forEach(c -> System.out.println(c.getTitle()));
+
+
+
+
      /*   for (ExampleCSV c : comics) {
             System.out.println(c);
         }   */
 
 
 
-        staticFiles.location("/public");
+       staticFiles.location("/public");
 
 
         get("/search", (req, res) -> {
@@ -155,16 +226,30 @@ public class ExampleCSV {
 
 
 
-        get("/query", (req, res) ->{
+
+
+       post("/query", (req, res) ->{
 
             Map<String, Object> model = new HashMap<String, Object>();
+            MultipartConfigElement multipartConfigElement = new MultipartConfigElement("C:\\Files\\XKCD.csv");
+            req.raw().setAttribute("org.eclipse.jetty.multipartConfig", multipartConfigElement);
 
-            for(ExampleCSV c : comics){
+          String string =  req.queryParams("term");
+
+//System.out.println(string);
+
+          /*  for(ExampleCSV c : testList){         // for each loop is not needed to get data
                 model.put("comics", comics);
-            }
+            }       */
+
+           List<ExampleCSV>  output = comics.stream()
+                   .filter(comic -> comic.getTitle().contains(string) )
+                   .collect(Collectors.toList());
+
+            model.put("comics", output);
 
 
-            return new JadeTemplateEngine().render(new ModelAndView(model, "comics"))  ;
+            return new JadeTemplateEngine().render(new ModelAndView(model, "comics"))    ;
 
 
         });
@@ -173,7 +258,3 @@ public class ExampleCSV {
     }
 
 
-
-
-
-}
